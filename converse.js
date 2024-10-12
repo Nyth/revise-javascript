72b11967fcb1561247649874f5443edcd3c356ad
Nayem-Al-Tareq <nayemaltareq786@gmail.com>
2024-10-12 14:06:51 -0230

    add

---
+++ converse.js
@@ -0,0 +1,11 @@
+var orange = 20;
+var applePrice = parseInt('212.56454')
+console.log(applePrice);
+
+var first = 0.1
+var second = 0.2
+var total = first + second
+console.log(total.toFixed(2))
+
+const num = 50 / 0;
+console.log(num);
